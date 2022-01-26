3a37252ecd9dfe69a6e1b49bc6a514068703db21
Volodymyr Volodko <volodymyr.volodko@gmail.com>
2022-01-26 15:23:24 +0200

    LL-38 add product name, thumnail and fileExtensions to favorites output

--- aspnet-core/src/layoutlovers.Application/Favorites/Dto/FavoriteDto.cs
+++ aspnet-core/src/layoutlovers.Application/Favorites/Dto/FavoriteDto.cs
@@ -1,10 +1,17 @@
 ﻿using Abp.AutoMapper;
 using layoutlovers.Favorites.Dto.Base;
+using layoutlovers.Files;
+using layoutlovers.Files.Dto;
+using System.Collections.Generic;
 
 namespace layoutlovers.Favorites.Dto
 {
     [AutoMap(typeof(Favorite))]
     public class FavoriteDto: FavoriteEntity
     {
+        public string ProductName { get; set; }
+        public S3ImageDto Thumbnail { get; set; }
+        public IEnumerable<FileType> FileExtension { get; set; }
+
     }
 }
